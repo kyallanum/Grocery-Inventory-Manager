53d4c845a109f51d0170d0b1a6387c1ecddd6d88
kyallanum <v-kylanu@microsoft.com>
2023-03-10 22:29:45 -0700

    Added inventory and item models

---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery_Inventory_Manager.Models
+{
+    public partial class Item
+    {
+        [JsonProperty("id", Required = Required.Always)]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Id { get; set; }
+
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+
+        [JsonProperty("qty", Required = Required.Always)]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Qty { get; set; }
+    }
+}
